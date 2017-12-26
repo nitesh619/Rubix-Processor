660ff82d3d27e5e30c7b455ca02f4d5ea05a2edd
nitesh jain <nitesh619.jn@gmail.com>
2017-12-26 23:34:13 +0530

    Color calibrated | 3D Cube plotting | noise filter and refactoring

--- src/ColorObject.py
+++ src/ColorObject.py
@@ -1,36 +1,34 @@
 import cv2
 import numpy as np
 
+
 class ColorObject(object):
     kernel = np.ones((5, 5), np.uint8)
 
     def __init__(self, color, color_lower, color_upper):
         self.color = color
-        self.color_lower = np.array(color_lower)
-        self.color_upper = np.array(color_upper)
+        self.color_lower = np.array(color_lower, np.uint8)
+        self.color_upper = np.array(color_upper, np.uint8)
 
     def getColorMask(self, image_hsv):
         mask = cv2.inRange(image_hsv, self.color_lower, self.color_upper)
         opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, ColorObject.kernel, iterations=1)
         return opening
 
-    def getColorContours(self, image_hsv):
-        mask = self.getColorMask(image_hsv)
-        return cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[1]
+    def getColorContours(self, image):
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = self.getColorMask(hsv)
+        contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
+        return self.filterContors(contours)
 
-    def detect(self, c):
+    def detectShape(self, c):
         # initialize the shape name and approximate the contour
         shape = "unidentified"
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.04 * peri, True)
 
         # if the shape is a triangle, it will have 3 vertices
-        if len(approx) == 3:
-            shape = "triangle"
-
-        # if the shape has 4 vertices, it is either a square or
-        # a rectangle
-        elif len(approx) == 4:
+        if len(approx) == 4:
             # compute the bounding box of the contour and use the
             # bounding box to compute the aspect ratio
             (x, y, w, h) = cv2.boundingRect(approx)
@@ -40,28 +38,26 @@ class ColorObject(object):
             # equal to one, otherwise, the shape is a rectangle
             # shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
             shape = 'rectangle'
-
-        # if the shape is a pentagon, it will have 5 vertices
-        elif len(approx) == 5:
-            shape = "pentagon"
-
-        # otherwise, we assume the shape is a circle
-        else:
-            shape = "circle"
-
         # return the name of the shape
         return shape
 
-    def detectAndDraw(self, image):
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        contours = self.getColorContours(hsv)
+    def filterContors(self, contours):
+        new_contours = []
         for c in contours:
-            shape = self.detect(c)
+            shape = self.detectShape(c)
             area = cv2.contourArea(c)
-            if 2000 < area < 15000 and shape == 'rectangle':
-                x, y, w, h = cv2.boundingRect(c)
-                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 0), 2)
-                cv2.putText(image, self.color, (x + 10, y + 44), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255))
+            if 2000 < area < 20000 and shape == 'rectangle':
+                new_contours.append(c)
+        return new_contours
+
+    def detectAndDraw(self, image, contours=None):
+        if contours is None:
+            contours = self.getColorContours(image)
+
+        for c in contours:
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 0), 2)
+            cv2.putText(image, self.color, (x + 10, y + 44), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255))
 
 
 if __name__ == "__main__":
--- src/RubixDetector.py
+++ src/RubixDetector.py
@@ -2,33 +2,44 @@ import urllib.request
 
 import cv2
 import numpy as np
-
+import winsound
 import ColorObject as cod
+import time
 
-# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 url = 'http://192.168.0.100:8080/shot.jpg'
+
+yellowDetector = cod.ColorObject('yelow', [20, 150, 150], [40, 255, 255])
+orangeDetector = cod.ColorObject('Orng', [5, 150, 150], [15, 235, 250])
+blueDetector = cod.ColorObject('Blue', [110, 50, 50], [120, 255, 255])
+greenDetector = cod.ColorObject('Green', [45, 50, 50], [75, 255, 255])
+redDetector = cod.ColorObject('Red', [120, 120, 140], [180, 250, 200])
+whiteDetector = cod.ColorObject('White', [70, 0, 180], [180, 120, 255])
 
 
 def getRubixImage():
-    # _, rcube = cap.read()
-    req = urllib.request.urlopen(url)
-    rcube_array = np.array(bytearray(req.read()), dtype=np.uint8)
-    img = cv2.imdecode(rcube_array, -1)
-    return cv2.resize(img, None, fx=.5, fy=.5, interpolation=cv2.INTER_CUBIC)
+    _, img = cap.read()
+    # req = urllib.request.urlopen(url)
+    # rcube_array = np.array(bytearray(req.read()), dtype=np.uint8)
+    # img = cv2.imdecode(rcube_array, -1)
+    return img  # cv2.resize(rcube, None, fx=1, fy=1, interpolation=cv2.INTER_CUBIC)
 
 
+start_time = time.time()
+end_time = 0
 while True:
     rcube = getRubixImage()
+    no_of_contour = 0
+    for detector in (whiteDetector, blueDetector, redDetector, greenDetector, yellowDetector, orangeDetector):
+        contours = detector.getColorContours(rcube)
+        no_of_contour = no_of_contour + len(contours)
+        detector.detectAndDraw(rcube, contours)
+        end_time = time.time()
 
-    yellowDetector = cod.ColorObject('yelow', [20, 150, 150], [40, 255, 255])
-    orangeDetector = cod.ColorObject('Orng', [10, 120, 130], [20, 255, 255])
-    blueDetector = cod.ColorObject('Blue', [110, 50, 50], [120, 255, 255])
-    greenDetector = cod.ColorObject('Green', [45, 50, 50], [75, 255, 255])
-    redDetector = cod.ColorObject('Red', [0, 150, 70], [10, 255, 255])
-    whiteDetector = cod.ColorObject('White', [0, 0, 150], [2, 50, 255])
-
-    for detector in (blueDetector, redDetector, greenDetector, yellowDetector, orangeDetector, whiteDetector):
-        detector.detectAndDraw(rcube)
+    if no_of_contour == 9 and (end_time - start_time) > 10:
+        winsound.Beep(2500, 1000)
+        start_time = time.time()
+        time.sleep(5)
 
     cv2.imshow('Rubix\'s Cube', rcube)
 
@@ -37,4 +48,4 @@ while True:
         break
 
 # cap.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWsindows()
